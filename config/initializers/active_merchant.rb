d5e50942822de2ea58e362f43013188d1745fd9d
Marc Clifton <marc.clifton@gmail.com>
2013-09-30 14:45:10 -0400

    Changed to USD.

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,2 +1,2 @@
-ActiveMerchant::Billing::PaypalExpressGateway.default_currency = 'BRL'
+ActiveMerchant::Billing::PaypalExpressGateway.default_currency = 'USD'
 ActiveMerchant::Billing::Base.mode = :test if (PaymentEngines.configuration[:paypal_test] == 'true' rescue nil)
